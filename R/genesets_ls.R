f8eb0bd650842069fc4beef01fea9cd1d596235d
Gabriel Odom <gjo15@miami.edu>
2017-10-31 19:05:50 -0400

    Added genesets list as a data file, but I can't complete the documentation until I find the source of the gene pathway sets.

---
+++ R/genesets_ls.R
@@ -0,0 +1,17 @@
+#'  Gene Pathway Sets
+#'
+#' @description Gene Pathway Sets from <WEBSITE HERE>
+#'
+#' @format A list of three elements:
+#' \itemize{
+#'   \item{pathways : }{A list of 1304 character vectors. Each vector contains
+#'     the names of the individual genes within that pathway as a vector of
+#'     character strings.}
+#'   \item{TERMS : }{A character vector of length 1304 containing the names of
+#'     the gene pathways.}
+#'   \item{setsize : }{A named integer vector of length 1304 containing the
+#'     number of genes in each gene pathway.}
+#' }
+#'
+#' @source SOURCE HERE
+"genesets_ls"
