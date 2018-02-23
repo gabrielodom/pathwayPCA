c562333a505febfb3a73bc69a8b149f19f893cf0
Gabriel J. Odom <gjo15@med.miami.edu>
2018-02-23 18:14:48 -0500

    I curated a smaller data set with ~650 features, and a smaller pathway set of 15 pathways. Because of this, the vignette builds in less than a minute, and I removed the --no-build-vignettes option from the Check.
    Further, I deleted all the large data sets and gene set lists, because I don't need them anymore.

---
+++ R/data_colonSubset.R
@@ -0,0 +1,10 @@
+#'  Colon Cancer -Omics Data
+#'
+#' @description Subset of colon cancer surival data set
+#'
+#' @format A subset of a data frame with 656 genes measured on 250 subjects.
+#'   The first two columns are the Overall Survival time (\code{OS_time}) and
+#'   death indicator (\code{OS_event}).
+#'
+#' @source Xi Steven Chen
+"colonSurv_df"
