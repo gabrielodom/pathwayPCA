ab3247f97ab733c46fdcd4e9744485dceac66615
Gabriel J. Odom <gjo15@med.miami.edu>
2018-03-08 14:49:09 -0500

    The end user no longer has to include the set size in the pathwaySet / pathwaySet_ls objects. We have updated the help files and example gene set list to reflect this. Now, all set size calculations are done at `Omics*` object generation (within the `create_Omics*() function calls).
    Further, I've added (and tested) hyperlinks between all of the class Rd files and the creation functions for those classes.

--- R/createClass_OmicsReg.R
+++ R/createClass_OmicsReg.R
@@ -4,14 +4,29 @@
 #'   "OmicsPathway" master class.
 #'
 #' @slot massSpec An $N x p$ data frame with named columns
-#' @slot pathwaySet A list of known gene pathways
+#' @slot pathwaySet A list of known gene pathways with one or two elements:
+#' \itemize{
+#'   \item{pathways : }{A named list of character vectors. Each vector contains
+#'     the names of the individual genes within that pathway as a vector of
+#'     character strings. The names contained in these vectors must have non-
+#'     empty overlap with the \emph{column names} of the \code{massSpec} data
+#'     frame. The names of the pathways (the list elements themselves) should
+#'     be the a shorthand representation of the full pathway name.}
+#'   \item{TERMS: }{ A character vector the same length as the
+#'     \code{pathways} list with the proper names of the pathways.}
+#'   \item{setsize : }{A named integer vector the same length as the
+#'     \code{pathways} list with the number of genes in each pathway. This list
+#'     item is calculated during the creation step of a \code{create_Omics*()}
+#'     function call.}
+#' }
 #' @slot response A numeric vector of length $N$: the dependent variable in a
 #'   regression exercise
 #'
 #' @importFrom methods new
 #'
 #' @include createClass_OmicsPath.R createClass_validOmics.R
-#' @seealso \code{"\link[=OmicsPathway-class]{OmicsPathway}"}
+#' @seealso \code{"\link[=OmicsPathway-class]{OmicsPathway}"},
+#'   \code{\link{create_OmicsReg}}
 #'
 #' @export
 setClass("OmicsReg",
