5980017c2dffb0838600881c432212c557b43feb
Gabriel J. Odom <gjo15@med.miami.edu>
2018-04-13 15:04:08 -0400

    Added accessor functions to the package. They are documented, and I tested them. Also, I'm finished with the second vignette.
    I also finally found a clue as to why I was getting the documentation error for the superPC_permuteSamples.R file. I'll dig into that next.

--- testing_files/Test_Omics_accessor_functions.R
+++ testing_files/Test_Omics_accessor_functions.R
@@ -1,4 +1,5 @@
-test_OS <- colon_OmicsSurv
+colon_OmicsPath <- create_OmicsPath(assayData_df = colonSurv_df[, -(1:2)],
+                                    pathwaySet_ls = colon_pathwaySet)
 
 ######  Create Acessor Methods  ###############################################
 
@@ -14,7 +15,7 @@ setMethod(f = "getAssay", signature = "OmicsPathway",
           })
 
 # Test
-getAssay(test_OS)
+getAssay(colon_OmicsSurv)
 
 
 ###  pathwaySet  ###
@@ -29,7 +30,7 @@ setMethod(f = "getPathwaySet", signature = "OmicsPathway",
           })
 
 # Test
-getPathwaySet(test_OS)
+getPathwaySet(colon_OmicsSurv)
 
 
 ###  Event Time  ###
@@ -44,7 +45,10 @@ setMethod(f = "getEventTime", signature = "OmicsSurv",
           })
 
 # Test
-getEventTime(test_OS)
+getEventTime(colon_OmicsPath)
+getEventTime(colon_OmicsSurv)
+getEventTime(colon_OmicsReg)
+getEventTime(colon_OmicsCateg)
 
 
 ###  Event Indicator  ###
@@ -59,7 +63,10 @@ setMethod(f = "getEvent", signature = "OmicsSurv",
           })
 
 # Test
-getEvent(test_OS)
+getEvent(colon_OmicsPath)
+getEvent(colon_OmicsSurv)
+getEvent(colon_OmicsReg)
+getEvent(colon_OmicsCateg)
 
 
 ###  Response Vector  ###
@@ -74,13 +81,19 @@ setMethod(f = "getResponse", signature = "OmicsPathway",
           })
 
 # Test
-getResponse(test_OS)
-# WRITE A FULL TEST.
+getResponse(colon_OmicsPath)
+getResponse(colon_OmicsSurv)
+getResponse(colon_OmicsReg)
+getResponse(colon_OmicsCateg)
 
 
 
 ######  Create Replacement Methods  ###########################################
+test_OP <- colon_OmicsPath
 test_OS <- colon_OmicsSurv
+test_OR <- colon_OmicsReg
+test_OC <- colon_OmicsCateg
+
 
 ###  Assay  ###
 setGeneric("getAssay<-",
@@ -105,7 +118,6 @@ getAssay(test_OS) <- data.frame(a = 12)
 getAssay(test_OS) <- data.frame(a = rep(1, 250))
 
 
-
 ###  pathwaySet  ###
 setGeneric("getPathwaySet<-",
            function(object, value){
@@ -126,3 +138,77 @@ setMethod(f = "getPathwaySet<-", signature = "OmicsPathway",
 # Test
 getPathwaySet(test_OS) <- 12
 getPathwaySet(test_OS) <- list(a = 12)
+
+
+###  Event Time  ###
+setGeneric("getEventTime<-",
+           function(object, value){
+             standardGeneric("getEventTime<-")
+           })
+
+setMethod(f = "getEventTime<-", signature = "OmicsSurv",
+          definition = function(object, value){
+
+            object@eventTime <- value
+
+            if(validObject(object)){
+              return(object)
+            }
+
+          })
+
+# Test
+getEventTime(test_OS) <- 12
+getEventTime(test_OS) <- rep(12, 250)
+
+
+###  Event Indicator  ###
+setGeneric("getEvent<-",
+           function(object, value){
+             standardGeneric("getEvent<-")
+           })
+
+setMethod(f = "getEvent<-", signature = "OmicsSurv",
+          definition = function(object, value){
+
+            object@eventObserved <- value
+
+            if(validObject(object)){
+              return(object)
+            }
+
+          })
+
+# Test
+getEvent(test_OS) <- 12
+getEvent(test_OS) <- TRUE
+getEvent(test_OS) <- rep(TRUE, 250)
+
+
+###  Response Vector  ###
+setGeneric("getResponse<-",
+           function(object, value){
+             standardGeneric("getResponse<-")
+           })
+
+setMethod(f = "getResponse<-", signature = "OmicsPathway",
+          definition = function(object, value){
+
+            object@response <- value
+
+            if(validObject(object)){
+              return(object)
+            }
+
+          })
+
+# Test
+getResponse(test_OP) <- 12
+getResponse(test_OS) <- 12
+getResponse(test_OR) <- 12
+getResponse(test_OC) <- 12
+
+getResponse(test_OR) <- rep(12, 250)
+getResponse(test_OC) <- as.factor(12)
+
+getResponse(test_OC) <- as.factor(rep(12, 250))
