1c2ba5c871b7654ca7ce98d7c4ea5056e7a02e48
Gabriel J. Odom <gjo15@med.miami.edu>
2018-05-02 16:31:47 -0400

    Updated the README. Added ?pathwayPCA to the documentation.

---
+++ R/pathwayPCA.R
@@ -0,0 +1,25 @@
+#' @name pathwayPCA
+#'
+#' @title Extract and Test the Significance of Pathway-Specific Principal
+#'    Components
+#'
+#' @description The \code{pathwayPCA} package has three main components:
+#'    \itemize{
+#'       \item{Import and Tidy Data: }{
+#'         \url{https://gabrielodom.github.io/pathwayPCA/articles/C2-Importing_Data.html}
+#'       }
+#'       \item{Create \code{Omics} Data Objects }{
+#'         \url{https://gabrielodom.github.io/pathwayPCA/articles/C3-Create_Omics_Objects.html}
+#'       }
+#'       \item{Test Pathway Significance }{
+#'         \url{https://gabrielodom.github.io/pathwayPCA/articles/C4-Methods_Walkthrough.html}
+#'       }
+#'     }
+#'   For an overview of these four pieces in context, please see our Quickstart
+#'   Guide:
+#'   \url{https://gabrielodom.github.io/pathwayPCA/articles/C1-Quickstart_Guide.html}
+#'
+#' @docType package
+#' @rdname pathwayPCA-package
+
+NULL
