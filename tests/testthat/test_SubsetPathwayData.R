4a0f79d5f38a699c1a4df6c0d936eea8a18a4ba4
Lizhong Liu <lxl816@miami.edu>
2019-03-28 14:51:07 -0400

    finish adding unit test scripts per issue #71

--- tests/testthat/test_SubsetPathwayData.R
+++ tests/testthat/test_SubsetPathwayData.R
@@ -1,24 +1,33 @@
 context("SubsetPathwayData")
 
-gitHubPath_char <-
-  "https://raw.githubusercontent.com/lizhongliu1996/pathwayPCAdata/master/"
-colon_aespcOut <- readRDS(
-  url(paste0(gitHubPath_char, "colon_aespcOut.rds"))
+data("colonSurv_df")
+data("colon_pathwayCollection")
+
+colon_OmicsSurv <- CreateOmics(
+  assayData_df = colonSurv_df[, -c(2:3)],
+  pathwayCollection_ls = colon_pathwayCollection,
+  response = colonSurv_df[, 1:3],
+  respType = "s"
 )
 
 test_that("SubsetPathwayData gives correct errors", {
   
-  
-  
-  expect_error(
-    SubsetPathwayData(), 
-    ""
+    expect_error(
+    SubsetPathwayData(
+      colon_OmicsSurv,
+      "haha"
+    ), "Pathway not found."
   )
   
+})
+
+test_that("SubsetPathwayData returns df with correct classes", {
   
-  
-  
-  
-  
+  expect_s3_class(
+    SubsetPathwayData(
+      colon_OmicsSurv,
+      "KEGG_RETINOL_METABOLISM"
+    ), c("tbl_df", "tbl", "data.frame")
+  )
   
 })
--- tests/testthat/test_getPathpVals.R
+++ tests/testthat/test_getPathpVals.R
@@ -6,19 +6,18 @@ colon_aespcOut <- readRDS(
   url(paste0(gitHubPath_char, "colon_aespcOut.rds"))
 )
 
-test_that("getPathpVals gives correct errors", {
+test_that("getPathpVals returns df with correct classes", {
   
-  
-  
-  expect_error(
-    getPathpVals(), 
-    ""
+  expect_s3_class(
+    getPathpVals(colon_aespcOut,
+                 numPaths = 5), 
+    class = c("tbl_df", "tbl", "data.frame")
   )
   
-  
-  
-  
-  
-  
+  expect_s3_class(
+    getPathpVals(colon_aespcOut,
+                 alpha = 0.01), 
+    class = c("tbl_df", "tbl", "data.frame")
+  )
   
 })
