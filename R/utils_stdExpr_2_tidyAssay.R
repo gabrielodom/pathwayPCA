7619eae22b4f30e08d0ace93bbc2be1b4406e81e
Gabriel J. Odom <gjo15@med.miami.edu>
2019-07-11 09:48:08 -0400

    Per Issue #83. Now we wait for the Bioconductor build report...

--- R/utils_stdExpr_2_tidyAssay.R
+++ R/utils_stdExpr_2_tidyAssay.R
@@ -35,7 +35,7 @@
 SE2Tidy <- function(summExperiment, whichAssay = 1){
   
   ###  Assay  ###
-  assay_mat <- slot(summExperiment, "assays")[[whichAssay]]
+  assay_mat <- slot(summExperiment, "assays")$data[[whichAssay]]
   dimnames(assay_mat)[c(1,2)] <- dimnames(summExperiment)
   assay_df <- TransposeAssay(
     assay_df = data.frame(assay_mat),
